2bdf80c88a456afcb03dc1f4099b87ecfebe72d9
chohee Lee <139777677+zdtxfy909@users.noreply.github.com>
2023-11-09 18:25:42 +0900

    Add files via upload

---
+++ 08_BoardService_log/src/main/java/com/spring/biz/board/impl/BoardServiceImpl.java
@@ -0,0 +1,64 @@
+package com.spring.biz.board.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.biz.board.BoardService;
+import com.spring.biz.board.BoardVO;
+import com.spring.biz.common.Log4jAdvice;
+import com.spring.biz.common.LogAdvice;
+
+// @Service : @Component 상속 확장 어노테이션
+//		비즈니스 로직 처리 서비스 영역에 사용
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+	@Autowired //타입이 일치하는 객체(인스턴스) 주입
+	private BoardDAO boardDAO;
+	//private LogAdvice log;
+	private Log4jAdvice log4j;
+	
+	//기본생성자 만들어서 언제 객체가 생성되는지 확인
+	public BoardServiceImpl() {
+		System.out.println(">> BoardServiceImpl() 객체 생성");
+		//log = new LogAdvice();
+		log4j = new Log4jAdvice();
+	}
+	
+	@Override
+	public void insertBoard(BoardVO vo) {
+		//log.printLog();
+		log4j.printLogging();
+		boardDAO.insertBoard(vo);
+	}
+
+	@Override
+	public void updateBoard(BoardVO vo) {
+		//log.printLog();
+		log4j.printLogging();
+		boardDAO.updateBoard(vo);
+	}
+
+	@Override
+	public void deleteBoard(BoardVO vo) {
+		//log.printLog();
+		log4j.printLogging();
+		boardDAO.deleteBoard(vo);
+	}
+
+	@Override
+	public BoardVO getBoard(BoardVO vo) {
+		//log.printLog();
+		log4j.printLogging();
+		return boardDAO.getBoard(vo);
+	}
+
+	@Override
+	public List<BoardVO> getBoardList() {
+		//log.printLog();
+		log4j.printLogging();
+		return boardDAO.getBoardList();
+	}
+
+}
---
+++ 08_BoardService_log/src/main/java/com/spring/biz/common/LogAdvice.java
@@ -0,0 +1,8 @@
+package com.spring.biz.common;
+
+public class LogAdvice {
+	
+	public void printLog() {
+		System.out.println("[공통로그] 비즈니스 로직 수행전 로그");
+	}
+}
